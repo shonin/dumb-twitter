4c5993637066743f5968191c01c49fc1b755c64b
Elliot Charney <elliotcharney@gmail.com>
2019-11-21 22:09:54 -0800

    add beginning of password compromisation checker

--- dumb_twitter/static/js/project.js
+++ dumb_twitter/static/js/project.js
@@ -1,14 +1,32 @@
 
 class TestPassword {
     constructor() {
-        console.log("foo bar");
-    }
-}
+        this.form = document.getElementById("login_form");
+        this.preventFormSubmit();
 
-if(document.getElementById("id_test_password").checked) {
-    TestPassword();
+
+    }
+
+    preventFormSubmit() {
+        this.form.addEventListener("submit", (e) => {
+            e.preventDefault();
+            this.addPasswordInformationOnSubmit();
+        })
+    }
+
+    addPasswordInformationOnSubmit() {
+        document.getElementById("password_card").classList.remove("hidden");
+    }
 }
 
 // set event listen on the checkbox
 // when checked, prevent default on submit
 // instead hash the password
+
+let checkbox = document.getElementById("id_test_password");
+
+checkbox.addEventListener( 'change', function() {
+    if(this.checked) {
+        new TestPassword();
+    }
+});
