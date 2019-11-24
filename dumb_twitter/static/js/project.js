ab565ab8dd720673913ea49021b1950ba307c53d
Elliot Charney <elliotcharney@gmail.com>
2019-11-23 19:37:14 -0800

    compromised password feature working on sign in page

--- dumb_twitter/static/js/project.js
+++ dumb_twitter/static/js/project.js
@@ -3,15 +3,21 @@ import sha1 from 'js-sha1';
 class TestPassword {
     constructor() {
         this.form = document.getElementById("login_form");
-        this.preventFormSubmit();
+        this.formHandler = this.preventFormSubmit.bind(this);
+
+        // learning moment: the scope of this.preventFormSubmit, when actually run
+        // is within the document.body -- when run 'this' will refer to the form.
+        // by binding this to the class function it prevents this from happening.
+        this.form.addEventListener("submit", this.formHandler);
+
+        // this.preventFormSubmit();
 
     }
 
-    preventFormSubmit() {
-        this.form.addEventListener("submit", (e) => {
-            e.preventDefault();
-            this.startPasswordCheck();
-        })
+    preventFormSubmit(e) {
+        console.log("preventFormSubmit run");
+        e.preventDefault();
+        this.startPasswordCheck();
     }
 
     startPasswordCheck() {
@@ -24,7 +30,6 @@ class TestPassword {
 
         const url = `https://api.pwnedpasswords.com/range/${first_five}`;
 
-        console.log(url);
         fetch(url)
             .then( data => data.text() )
             .then( (res) => {
@@ -47,7 +52,7 @@ class TestPassword {
     }
 
     giveUserResults(isPasswordCompromised, compromisedSha1) {
-        let header, body, class1, class2;
+        let header, body, class1, class2, removeClass;
 
         if(isPasswordCompromised) {
             let timesCompromised = compromisedSha1.slice(compromisedSha1.indexOf(':')+1);
@@ -55,23 +60,32 @@ class TestPassword {
             header = "Warning";
             body =
                 `Your password has been involved in ${timesCompromised} data breach(es). 
-                <a href="https://haveibeenpwned.com/Passwords" target="_blank">Learn more here</a>.`;
+                <a href="https://haveibeenpwned.com/Passwords" target="_blank">Learn more here</a>.
+                <br/>Submit the form again to continue.`;
             class1 = "bg-danger";
             class2 = "text-white";
+
+            removeClass = "bg-success";
         } else {
             header = "No password breach detected";
             body =
-                `<a href="https://haveibeenpwned.com/Passwords" target="_blank">Learn more here</a>.`;
+                `<a href="https://haveibeenpwned.com/Passwords" target="_blank">Learn more here</a>.
+                 <br/>Submit the form again to continue.`;
             class1 = "bg-success";
             class2 = "text-white";
+
+            removeClass = "bg-danger";
         }
 
+        if(document.querySelector("#password_card").classList.contains(removeClass)) {
+            document.querySelector("#password_card").classList.remove(removeClass);
+        }
 
         document.querySelector("#password_card .card-title").innerHTML = header;
         document.querySelector("#password_card .card-text").innerHTML = body;
         document.querySelector("#password_card").classList.add(class1, class2);
 
-        this.form.removeEventListener("submit", () => {} );
+        this.form.removeEventListener("submit", this.formHandler);
     }
 }
 
@@ -86,3 +100,5 @@ checkbox.addEventListener( 'change', function() {
         new TestPassword();
     }
 });
+
+
