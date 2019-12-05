bcace32c3199495d7764e5941cbf86a1a2ca2950
Elliot Charney <elliotcharney@gmail.com>
2019-12-04 21:57:55 -0800

    remove stray code from password branch

--- dumb_twitter/static/js/project.js
+++ dumb_twitter/static/js/project.js
@@ -1,14 +1,2 @@
 
-class TestPassword {
-    constructor() {
-        console.log("foo bar");
-    }
-}
-
-if(document.getElementById("id_test_password").checked) {
-    TestPassword();
-}
-
-// set event listen on the checkbox
-// when checked, prevent default on submit
-// instead hash the password
+console.log("foo bar");
